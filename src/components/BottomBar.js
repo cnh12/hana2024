276b40962d818483b6e9329ce2252b35f1d2e1bd
CNH <zetta_byte@naver.com>
2024-05-19 11:57:25 +0900

    이름 변경

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 function BottomBar() {
   return (
     <div className="bottom-bar">
-      <a>ⓒ 2024.조남현 all right reserved.</a>             
+      <a>행내한이니 유출/공유에 각별히 주의해주세요.</a>             
     </div>
   );
 }
